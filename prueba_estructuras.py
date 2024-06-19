ed0e18aab484767bdf07e16520c4a20316a5d9ea
Cristopher Madrigal <cristopher.madrigal.chavarria@est.una.ac.cr>
2024-06-19 13:32:52 -0600

    convertir a .py y agregar lista

--- prueba_estructuras.py
+++ prueba_estructuras.py
@@ -6,3 +6,11 @@ def proceso(lista):
         else:
             resultado.append(lista[i]* 2)
     return resultado
+
+lista_ejemplo = [1,2,3,4,5]
+
+print(proceso(lista_ejemplo))
+
+"""
+Prueba modificación y convertir a documento python
+"""
